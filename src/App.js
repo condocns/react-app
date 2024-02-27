f922318d98a8a2309ebc0960c4ca81e3a110c95d
Nawin Singgun <nawin_s@toptier.co.th>
2024-02-27 10:38:46 +0700

    Formatting code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          "CI/CD to EC2"
-        </p>
+        <p>"CI/CD to EC2"</p>
         <a
           className="App-link"
           href="https://reactjs.org"
