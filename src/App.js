59aa4fc01a981415e159c0990727a5e6761fbf42
Nawin Singgun <nawin_s@toptier.co.th>
2024-02-27 15:24:58 +0700

    change txt

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>"CI/CD for EC2"</p>
+        <p>"Test CI/CD reactjs"</p>
         <a
           className="App-link"
           href="https://reactjs.org"
