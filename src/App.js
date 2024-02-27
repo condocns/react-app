afbba1a95009a3c5e1acbe9e893ce3d619b54176
Nawin Singgun <nawin_s@toptier.co.th>
2024-02-27 13:33:30 +0700

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>"CI/CD to EC2"</p>
+        <p>"CI/CD for EC2"</p>
         <a
           className="App-link"
           href="https://reactjs.org"
