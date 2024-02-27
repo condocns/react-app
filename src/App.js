1ddc31d69a60a225a2d038a66634a93427924ac4
Nawin Singgun <nawin_s@toptier.co.th>
2024-02-27 09:58:28 +0700

    Test Deploy to ec2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          "Welcome to CI/CD to EC2"
+          "CI/CD to EC2"
         </p>
         <a
           className="App-link"
